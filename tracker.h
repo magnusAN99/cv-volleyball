e5c994cb3c25407d87943c5d5aef0b53535a8400
Magnus <magnus.a.n@hotmail.com>
2026-04-24 12:05:54 +0200

    Move track matching logic from main.cpp to tracker.cpp

--- tracker.h
+++ tracker.h
@@ -3,6 +3,7 @@
 //
 #pragma once
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 struct Track {
     int id;
@@ -10,3 +11,16 @@ struct Track {
     int framesSinceLastSeen;
 };
 
+class Tracker {
+public:
+    void update(const std::vector<cv::Point2f>& detections);
+    std::vector<cv::Point2f> getActivePositions(int minAge = 5) const;
+
+private:
+    std::vector<Track> tracks_;
+    int nextId_ = 0;
+
+    float maxMatchDistance_ = 2.0f;
+    float alpha_ = 0.3f;
+    int maxStaleFrames_ = 30;
+};
